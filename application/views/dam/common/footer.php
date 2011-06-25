cd5c463d04d3d41ce8fdf25604b7d1c1368598b5
Andy Bright <andybright@gmail.com>
2011-06-25 17:19:45 +0000

    cleaned up upload page a bit

--- application/views/dam/common/footer.php
+++ application/views/dam/common/footer.php
@@ -5,7 +5,6 @@
 			<p>A nifty little web app by <a href="http://jamesconstable.co.uk/">James Constable</a> and <a href="http://andybright.ws/">Andy Bright</a> &copy;2008&ndash;2011</p>
 		</div>
     </div>
-
-            
+   
 </body>
 </html>
--- application/views/dam/content/images/upload.php
+++ application/views/dam/content/images/upload.php
@@ -1,32 +1,18 @@
 <div id="upload" class="boxWithPadding">
-    <h2>Upload An Image</h2>
+    <h2>Image Upload</h2>
 
-	<ol>
-        <li>
-			<ul>
-				<li>Press 'Add Files' to add image files to your upload queue.</li>
-				<li>Supported file formats are JPEG, PNG, GIF, PSD and TIFF at filesizes upto 50MB.</li>
-				<li>Once added to the queue click Upload to begin transferring the files from your computer to efstop. Do not navigate away from this page before the Upload has completed.</li>
-				<li>When the upload process has complete you will be able to edit and view the properties of your images.</p></li>
-			</ul>
-		</li>
-        
-        <li>
-        
-        <?=form_open_multipart($package.'/image/upload');?>
-                <fieldset id="fileUpload">
-				
-                <label for="imageupload">Select Your File</label>
-				<input type="file" name="imageupload" id="imageupload" />
-				
-				<input type="submit" name="submit" id="submit" value="Upload Image" />
-            </fieldset>
-        <?=form_close();?>
-        </li>
-        
-        <li><p class="uploaded">All your images are uploaded! <a href="<?=site_url($package.'/image/viewOrphans');?>">Edit their details</a>.</p></li>
-    </ol>
+	<p>Supported file formats are JPEG, PNG, GIF, PSD and TIFF at filesizes up to X MB.</p>
+	<p>Once added to the queue click Upload to begin transferring the files from your computer to efstop. Do not navigate away from this page before the Upload has completed.</p>
+	<p>When the upload process has complete you will be able to edit and view the properties of your images.</p>
 
- 
+	<?=form_open_multipart($package.'/image/upload');?>
+	<fieldset id="fileUpload">
+	
+		<label for="imageupload">Image to Upload</label>
+		<input type="file" name="imageupload" id="imageupload" />
 
+		<input class="button" type="submit" name="submit" id="submit" value="Upload" />
+		
+	</fieldset>
+	<?=form_close();?>
 </div>
